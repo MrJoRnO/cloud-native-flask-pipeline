a166aba00f383a8750e7e499e338953433d33130
Noam-Journo <noamjorno6@gmail.com>
2026-02-15 14:33:48 +0200

    full ci/cd-fix2

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,18 +5,21 @@ variable "aws_region" {
 
 variable "vpc_cidr" {
   type    = string
+  default = "10.0.0.0/16"
 }
 
 variable "cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
-  default     = "devops-assignment-eks" # הוספת השורה הזו פותרת את הבעיה
+  default     = "devops-assignment-eks"
 }
 
 variable "public_subnet_cidrs" {
   type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnet_cidrs" {
   type    = list(string)
+  default = ["10.0.11.0/24", "10.0.12.0/24"]
 }
